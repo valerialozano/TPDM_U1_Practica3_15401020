d558f5977ceec8224566a5dca76f92498cd769c6
valerialozano <carovalery@hotmail.com>
2019-09-24 20:15:31 -0600

    Initial commit

---
+++ app/src/main/java/mx/edu/ittepic/tpdm_u1_practica3_15401020/Main4Activity.kt
@@ -0,0 +1,12 @@
+package mx.edu.ittepic.tpdm_u1_practica3_15401020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Main4Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main4)
+    }
+}
